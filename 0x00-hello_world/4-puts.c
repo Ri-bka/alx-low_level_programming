1f3990bdb9bdacbf0fc1d5d6019507a6ade9fee1
Ri-bka <ribkagetu0@gmail.com>
2022-12-08 12:44:10 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,22 +2,12 @@
 #include <stdio.h>                                                     
 
 /**                                                                     
+ *main - Entry Point                           
  *
- *  *main - Entry Point                                                    
- *
- *   *                                                                      
- *
- *    *Return:  Always 0 (Success)                                           
- *
- *     */                                                                  
+ *Return:  Always 0 (Success)                                           
+ */                                                                  
 int main(void)                                                          
-
 {                                                                       
 	        puts("\"Programming is like building a multilingual puzzle");   
-
 		        return (0);                                                     
 }
-
-// click “esc” to re-enter command mode
-//
-//
